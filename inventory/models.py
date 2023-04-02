b1ca6d05c5184fa8252c501f603a849f245a4448
Jaeden Cruz <jaedencruz4@gmail.com>
2023-04-02 06:22:41 -0400

    second commit first models were created.

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=200)
+    price_per_unit = models.FloatField(default=0)
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    item_price = models.FloatField(default=0.00)
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    time_ordered = models.DateTimeField(auto_now_add=True)
